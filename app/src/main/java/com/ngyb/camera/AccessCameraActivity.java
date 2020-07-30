6dab417ae94221e3e77d71ea67118e19bd5dada4
南宫燚滨 <nangongyibin@gmail.com>
2020-07-30 16:44:11 +0800

    前置后置分开显示。

--- app/src/main/java/com/ngyb/camera/AccessCameraActivity.java
+++ app/src/main/java/com/ngyb/camera/AccessCameraActivity.java
@@ -52,9 +52,11 @@ public class AccessCameraActivity extends AppCompatActivity implements SurfaceHo
             Toast.makeText(this, "请开启存储或相机的权限", Toast.LENGTH_SHORT).show();
             return;
         }
+        // 后置
         surfaceHolder1 = sfv1.getHolder();
         surfaceHolder1.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         surfaceHolder1.addCallback(this);
+        //前置
         surfaceHolder2 = sfv2.getHolder();
         surfaceHolder2.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         surfaceHolder2.addCallback(this);
@@ -68,26 +70,31 @@ public class AccessCameraActivity extends AppCompatActivity implements SurfaceHo
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         try {
-            //获取camera对象
-            int cameraCount = Camera.getNumberOfCameras();
-            if (cameraCount > 0) {
-                //后置摄像头
-                camera1 = Camera.open(0);
-                //设置预览监听
-                camera1.setPreviewDisplay(holder);
-                Camera.Parameters parameters01 = camera1.getParameters();
-                if (getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
-                    parameters01.set("orientation", "portrait");
-                    camera1.setDisplayOrientation(90);
-                    parameters01.setRotation(90);
-                } else {
-                    parameters01.set("orientation", "landscape");
-                    camera1.setDisplayOrientation(0);
-                    parameters01.setRotation(0);
+            if (holder == surfaceHolder1) {
+                //获取camera对象
+                int cameraCount = Camera.getNumberOfCameras();
+                if (cameraCount > 0) {
+                    //后置摄像头
+                    camera1 = Camera.open(0);
+                    //设置预览监听
+                    camera1.setPreviewDisplay(holder);
+                    Camera.Parameters parameters01 = camera1.getParameters();
+                    if (getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+                        parameters01.set("orientation", "portrait");
+                        camera1.setDisplayOrientation(90);
+                        parameters01.setRotation(90);
+                    } else {
+                        parameters01.set("orientation", "landscape");
+                        camera1.setDisplayOrientation(0);
+                        parameters01.setRotation(0);
+                    }
+                    camera1.setParameters(parameters01);
+                    //启动摄像头预览
+                    camera1.startPreview();
                 }
-                camera1.setParameters(parameters01);
-                //启动摄像头预览
-                camera1.startPreview();
+            } else if (holder == surfaceHolder2) {
+                //获取camera对象
+                int cameraCount = Camera.getNumberOfCameras();
                 if (cameraCount > 1) {
                     //前置摄像头
                     camera2 = Camera.open(1);
@@ -111,32 +118,39 @@ public class AccessCameraActivity extends AppCompatActivity implements SurfaceHo
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        camera1.autoFocus(new Camera.AutoFocusCallback() {
-            @Override
-            public void onAutoFocus(boolean success, Camera camera) {
-                if (success) {
-                    initCamera(camera);//实现相机的参数初始化
-                    camera.cancelAutoFocus();//只有加上这一句，才会自动对焦
+        if (holder == surfaceHolder1) {
+            Log.e(TAG, "surfaceChanged: surfaceHolder1");
+            camera1.autoFocus(new Camera.AutoFocusCallback() {
+                @Override
+                public void onAutoFocus(boolean success, Camera camera) {
+                    if (success) {
+                        initCamera(camera);//实现相机的参数初始化
+                        camera.cancelAutoFocus();//只有加上这一句，才会自动对焦
+                    }
                 }
-            }
-        });
-        camera2.autoFocus(new Camera.AutoFocusCallback() {
-            @Override
-            public void onAutoFocus(boolean success, Camera camera) {
-                if (success){
-                    initCamera(camera);//实现相机的参数初始化
-                    camera.cancelAutoFocus();//只有加上这一句，才会自动对焦
+            });
+        } else if (holder == surfaceHolder2) {
+            Log.e(TAG, "surfaceChanged: surfaceHolder2");
+            camera2.autoFocus(new Camera.AutoFocusCallback() {
+                @Override
+                public void onAutoFocus(boolean success, Camera camera) {
+                    if (success) {
+//                        initCamera(camera);//实现相机的参数初始化
+                        camera.cancelAutoFocus();//只有加上这一句，才会自动对焦
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
     /**
      * 相机参数的初始化设置
+     *
      * @param sendCamera
      */
     private void initCamera(Camera sendCamera) {
